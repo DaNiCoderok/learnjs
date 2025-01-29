5e09be5b9621528caf77bcc139b7f182bb97d375
DaNiCoderok <danyalahmeduk@gmail.com>
2025-01-29 23:52:52 +0000

    01 updating

---
+++ 01_variables.js
@@ -0,0 +1,34 @@
+// we will learn about variables in js in this file
+
+// there are 3 types of variables
+
+// here are some examples
+
+// first is  >> var << variable
+// example is 
+var name = 'Dani'
+console.log(name);
+/*
+Defination: var is old type of variable to store the data but there were lots of issue with var keywords.
+so it got changed. everyone now recommended to use let to issue value.
+*/
+
+
+// second is  >> let << variable
+// example is 
+let myName = 'Daniname'
+console.log(myName);
+/*
+Defination: let is the new type of javascript variable in which you can take any value and store it . it can be change.
+if someone change the value in the project then it will be changed.
+ */
+
+
+// third is  >> const << variable
+// example
+const myDob = 1990
+console.log(myDob);
+/*
+Defination: const keyword meaning constant its mean it will never change throughout the project. even if someone want to change
+it cannot be changed. its not possible to change the value inside the const variable
+ */
